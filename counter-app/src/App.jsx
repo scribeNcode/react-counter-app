ec9d21bafa8a7a56b39ec642c0448f96e32a386e
scribencode <deyeye99@gmail.com>
2025-05-31 20:19:50 +0100

    initial commit create react counter app

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [number, setNumber] = useState(0);
+
+  const handleIncreament = () => {
+    if (number < 10) {
+      setNumber((prev) => prev + 1);
+    }
+  };
+
+  const handleDecreament = () => {
+    if (number > 0) {
+      setNumber((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <section className="counter-wrapper">
+      <div className="current-number-wrapper">
+        <p className="current-number">Number : {number} </p>
+      </div>
+      <div className="limit-alert-wrapper">
+        <p className="limit-alert">
+          {number === 10 && <p>You've reached the limit!</p>}
+        </p>
+      </div>
+      <div className="control-buttons-wrapper">
+        <div className="increase-button-wrapper">
+          <button onClick={handleIncreament}>+</button>
+        </div>
+        <div className="decrease-button-wrapper">
+          <button onClick={handleDecreament}>-</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
